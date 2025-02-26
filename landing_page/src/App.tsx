5af986217154fc8617a060bc6128677b4d299fa7
kumarpraveer143 <kumarpraveernit@gmail.com>
2025-02-26 15:32:25 +0530

    frontend build successfully!

--- landing_page/src/App.tsx
+++ landing_page/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./page/LandingPage";
 import Thanks from "./page/Thanks";
 
